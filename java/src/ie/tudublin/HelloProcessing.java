250286b28e84441f1c0c6ab3dc5b716111c063db
Fajar Albalushi <fajaralbalushi@Fajars-MacBook-Air.local>
2023-01-30 12:21:03 +0000

    deleted drawing code

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,7 +11,7 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		//colorMode(HSB);
 		background(0);
 
 		
@@ -20,6 +20,17 @@ public class HelloProcessing extends PApplet
 	
 	public void draw()
 	{	
-		
+		//background(255,0,0);
+		stroke(255);
+		line(10,10, 100, 100);//x1,y1,x2,y2
+		circle(300,250,70);//cx,cy,d
+		fill(50,100,0);
+		rect(10,300,20,100);//tlx,tly,w,h
+		stroke(127);
+		fill(241,254,0);
+		noStroke();
+		strokeWeight(1);
+		triangle(40,90,300,20,80,70);
+		fill(0,0,255);
 	}
 }
