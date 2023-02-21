a5bd2de7830608ef81dc073bbe2079de11dbfe50
Your Name <you@example.com>
2023-02-21 11:05:34 +0000

    grid

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,15 +1,9 @@
-
 package ie.tudublin;
 
 import processing.core.PApplet;
-import processing.data.TableRow;
-import java.util.ArrayList;
-import processing.data.Table;
 
 public class StarMap extends PApplet
 {
-
-    ArrayList<Star> stars = new ArrayList<Star>();
 
 	public void settings()
 	{
@@ -17,87 +11,50 @@ public class StarMap extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
 
-        Table table = loadTable("stars.csv", "header");
-    
-        for (TableRow row : table.rows()) {
-            Star star = new Star(row);
-            stars.add(star);
-        }
-
-        loadData("stars.csv");
-        loadData("stars.csv");
-        printStars();
-
-
-		smooth();
-	}
+        
 
 	
-	public void draw()
-	{	
-		drawGrid();
 
+		smooth();
+		
 	}
 
-	public class Star {
-         String DisplayName;
-         boolean isHabitable;
-         float distance;
-         float xG;
-         float yG;
-         float zG;
-         float absoluteMagnitude;
-    
-         public Star(Table table, int rowIndex) {
-            DisplayName = table.getString(rowIndex, "Display Name");
-            distance = table.getFloat(rowIndex, "Distance");
-            absoluteMagnitude = table.getFloat(rowIndex, "AbsMag");
-            xG = table.getFloat(rowIndex, "Xg");
-            yG = table.getFloat(rowIndex, "Yg");
-            zG = table.getFloat(rowIndex, "Zg");
+    public void drawGrid()
+    {
+        stroke(0,255,255);
+        float border = width * 0.1f;
+
+        
+        for(int i = -5; i <= 5; i++)
+        {
+            float x = map(i, -5, 5, border, width - border);
+            line(x, border, x, height - border);
+            line(border, x, width - border, x);
+
+            textAlign(CENTER, CENTER);
+            text(i, border * 0.5f, x);
+            text(i, x, border * 0.5f);
+
         }
 
-        public Star(TableRow row) {
-            this.DisplayName = row.getString("Display Name");
-            this.distance = row.getFloat("Distance");
-            // Assign the other properties in a similar way
-        }
-        
         
     }
 
-    public void loadData(String filename) {
-        Table table = loadTable(filename, "header");
-        for (TableRow row : table.rows()) {
-            Star star = new Star(row);
-            stars.add(star);
-        }
-    }
 
-    public void printStars() {
-        for (Star star : stars) {
-            println(star.toString());
-        }
-    }
+	public void draw()
+	{	
+		strokeWeight(1);
+		drawGrid();
+	}
 
-    public void drawGrid() {
-        stroke(150, 0, 255);
-        strokeWeight(1);
-        textSize(12);
-        for (int i = -5; i <= 5; i++) {
-            float x = map(i, -5, 5, 50, width - 50);
-            float y = map(i, -5, 5, height - 50, 50);
-            line(x, 50, x, height - 50);
-            line(50, y, width - 50, y);
-            textAlign(CENTER, TOP);
-            fill(150, 0, 255);
-            text(str(i) + " pc", x, height - 40);
-            textAlign(RIGHT, CENTER);
-            text(str(i) + " pc", 40, y);
-        }
-    }   
+    public void drawStars()
+    {
+        //for(Star s:stars()
+       // {
+          //  s.render(this);
+       // }
+    }
 }
-
